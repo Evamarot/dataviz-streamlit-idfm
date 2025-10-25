a21ed6c632ebf876ed7e644ff07bd75e64364ebd
Evamarot <eva.marot13@gmail.com>
2025-10-25 18:47:20 +0200

    modifs

--- sections/intro.py
+++ sections/intro.py
@@ -14,7 +14,7 @@ def render():
         [**Horaires prévus sur les lignes de transport en commun d'Ile-de-France (GTFS Datahub)**](https://www.data.gouv.fr/datasets/horaires-prevus-sur-les-lignes-de-transport-en-commun-dile-de-france-gtfs-datahub/) (Schedules), 
         provided by Île-de-France Mobilités via data.gouv.fr.
     """)
-    st.subheader("Our Data Story Arc:")
+    st.subheader("My Data Story Arc:")
     st.markdown("""
         1.  **The Big Picture:** What's the average air quality score, and how are stations distributed across pollution levels? (Overview & KPIs)
         2.  **Digging Deeper:** Where are the pollution hotspots geographically? Which specific lines tend to have higher pollution? Crucially, is there a direct link between a station's traffic volume and its pollution score? (Deep Dive)
